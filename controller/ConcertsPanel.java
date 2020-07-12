0b5a8c8b54e586db970de81bd028dc6ee8e6a50d
hatanakashumpei <hatanakapeter@gmail.com>
2020-07-12 20:50:31 +0900

    テーブルによってデータ表示を完成by hatanaka

--- controller/ConcertsPanel.java
+++ controller/ConcertsPanel.java
@@ -6,11 +6,15 @@ import lib.GUILibrary;
 import model.UserReader;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-
+import java.util.Vector;
+@SuppressWarnings("serial")
 public class ConcertsPanel extends JPanel implements ActionListener, Mediator {
     public static int ALL_PANEL_WIDTH = 900;
     public static int PANEL_WIDTH = 800;
@@ -45,17 +49,18 @@ public class ConcertsPanel extends JPanel implements ActionListener, Mediator {
 
         // JPanel for category
         JPanel categoryPanel = new JPanel();
-        categoryPanel.setPreferredSize(new Dimension(PANEL_WIDTH, 100));
+        categoryPanel.setPreferredSize(new Dimension(PANEL_WIDTH, 500));
 
         categoryPanel.add(new ConcertColumnLabel("Register"));
         categoryPanel.add(new ConcertColumnLabel("Title"));
         categoryPanel.add(new ConcertColumnLabel("Genre"));
         categoryPanel.add(new ConcertColumnLabel("Day"));
-        categoryPanel.add(new ConcertColumnLabel("Artist"));
+        categoryPanel.add(new ConcertColumnLabel("Place"));
         categoryPanel.add(new ConcertColumnLabel("Fee"));
         categoryPanel.add(new ConcertColumnLabel("Capacity"));
 
         //Concert List
+        /*
         listModel = new DefaultListModel<String>();
         var allConcerts = UserReader.getAllConcerts();
         for(var concert : allConcerts){
@@ -65,13 +70,45 @@ public class ConcertsPanel extends JPanel implements ActionListener, Mediator {
             }
             listModel.addElement(detail);
         }
-
         list = new JList<String>(listModel);
         list.setVisibleRowCount(10);
         list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         JScrollPane scrollPanel = new JScrollPane(list);
         scrollPanel.createVerticalScrollBar();
         scrollPanel.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPanel.setPreferredSize(new Dimension(PANEL_WIDTH, 100));
+        */
+
+        //Concert Table
+        String[] columnNames = {"Register","Title","Genre","Day","Place","Fee","Capacity"};
+        /*Test Data*/
+        Object[][] data = new Object[][]{{false, "Magicl Mirai 2020", "Vocaloid", "2020/08/10", "Makuhari Messe", "5600", "100"}};
+        JTable table = new JTable();
+        DefaultTableModel modeltable = new DefaultTableModel(columnNames,0){
+
+            public Class<?> getColumnClass(int columnIndex){
+                switch(columnIndex){
+                    case 0: return Boolean.class;
+                    default:
+                        return super.getColumnClass(columnIndex);
+                }
+            }
+        };
+        /*add All data for each row*/
+        modeltable.addRow(data[0]); //Test data push
+        /*format string in center*/
+        table.setModel(modeltable);
+        DefaultTableCellRenderer tableCellRenderer= new DefaultTableCellRenderer();
+        tableCellRenderer.setHorizontalAlignment(JLabel.CENTER);
+        for(int i=1;i<7;i++) {
+            TableColumn col = table.getColumnModel().getColumn(i);
+            col.setCellRenderer(tableCellRenderer);
+        }
+
+        /*set scroll*/
+        JScrollPane scrollTable = new JScrollPane(table);
+        scrollTable.setPreferredSize(new Dimension(PANEL_WIDTH,500));
+
 
 
         //register and logoutButton
@@ -86,9 +123,10 @@ public class ConcertsPanel extends JPanel implements ActionListener, Mediator {
 
         this.add(labelPanel);
         add(GUILibrary.getHr(800, 0));
-        this.add(categoryPanel, BorderLayout.CENTER);
-        add(GUILibrary.getHr(800, 0));
-        this.add(scrollPanel);
+        //this.add(categoryPanel, BorderLayout.CENTER);
+        //add(GUILibrary.getHr(800, 0));
+        //this.add(scrollPanel);
+        this.add(scrollTable);
         add(GUILibrary.getHr(800, 0));
         this.add(clickPanel);
     }
