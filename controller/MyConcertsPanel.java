9c274482bee8d49f7b39c6f612db0126757356db
yuziroppe <syani4732@gmail.com>
2020-07-18 01:31:28 +0900

    管理画面以外を作成

--- controller/MyConcertsPanel.java
+++ controller/MyConcertsPanel.java
@@ -1,9 +1,7 @@
 package controller;
 
 import components.ColleagueButton;
-import components.ConcertColumnLabel;
 import lib.GUILibrary;
-import model.UserReader;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
@@ -18,7 +16,7 @@ public class MyConcertsPanel extends JPanel implements ActionListener, Mediator 
     public static int ALL_PANEL_WIDTH = 900;
     public static int PANEL_WIDTH = 800;
     public static int PANEL_HEIGHT = 1000;
-    public static String[] COLUMN_NAMES = {"Delete","Title","Genre","Day","Place","Fee","Capacity"};
+    public static String[] COLUMN_NAMES = {"Delete", "Title", "Genre", "Day", "Place", "Fee", "Capacity"};
 
     private ColleagueButton buttonDelete;
     private ColleagueButton buttonAllConcerts;
@@ -47,21 +45,6 @@ public class MyConcertsPanel extends JPanel implements ActionListener, Mediator 
         subject.setFont(new Font("Arial", Font.PLAIN, 30));
         labelPanel.add(subject);
 
-        // JPanel for category
-        /*
-        JPanel categoryPanel = new JPanel();
-
-        categoryPanel.setPreferredSize(new Dimension(PANEL_WIDTH, 100));
-
-        categoryPanel.add(new ConcertColumnLabel("Register"));
-        categoryPanel.add(new ConcertColumnLabel("Title"));
-        categoryPanel.add(new ConcertColumnLabel("Genre"));
-        categoryPanel.add(new ConcertColumnLabel("Day"));
-        categoryPanel.add(new ConcertColumnLabel("Artist"));
-        categoryPanel.add(new ConcertColumnLabel("Fee"));
-        categoryPanel.add(new ConcertColumnLabel("Capacity"));
-        */
-
         //delete and logoutButton
         JPanel clickPanel = new JPanel();
 
@@ -75,18 +58,13 @@ public class MyConcertsPanel extends JPanel implements ActionListener, Mediator 
 
         this.add(labelPanel);
         add(GUILibrary.getHr(800, 0));
-        //this.add(categoryPanel, BorderLayout.CENTER);
         this.add(scrolleTable);
         add(GUILibrary.getHr(800, 0));
         this.add(clickPanel);
     }
 
-    public void initialize() {
-        var allConcerts = UserReader.getAllConcerts();
-        var myConcerts = this.mainFrame.getUserReader().getMyConcerts();
-
-        // FIXME: change the view
-        this.setMyConcertsTable();
+    public void reload() {
+        this.updateMyConcertsTable();
     }
 
     public void createColleagues() {
@@ -96,6 +74,29 @@ public class MyConcertsPanel extends JPanel implements ActionListener, Mediator 
 
         myConcertsTable = new JTable();
         scrolleTable = new JScrollPane(myConcertsTable);
+
+        DefaultTableCellRenderer tableCellRenderer = new DefaultTableCellRenderer();
+        tableCellRenderer.setHorizontalAlignment(JLabel.CENTER);
+
+        for (int i = 1; i < myConcertsTable.getColumnModel().getColumnCount(); i++) {
+            TableColumn col = myConcertsTable.getColumnModel().getColumn(i);
+            col.setCellRenderer(tableCellRenderer);
+        }
+
+        scrolleTable.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT / 2));
+
+        // フォーマット作成
+        DefaultTableModel modeltable = new DefaultTableModel(COLUMN_NAMES, 0) {
+            public Class<?> getColumnClass(int columnIndex) {
+                if (columnIndex == 0) {
+                    return Boolean.class;
+                }
+                return super.getColumnClass(columnIndex);
+            }
+        };
+
+        this.myConcertsTable.setModel(modeltable);
+
 
         buttonDelete.setMediator(this);
         buttonAllConcerts.setMediator(this);
@@ -108,18 +109,9 @@ public class MyConcertsPanel extends JPanel implements ActionListener, Mediator 
 
     public void colleagueChanged() {
         if (this.buttonDelete.nowAction()) {
-            // FIXME: should add checked concerts to myConcerts
-            //ArrayList<Integer> deletedConcertsIndexList = new ArrayList<>();
-
-            /*
-            for (var i : deletedConcertsIndexList) {
-                this.mainFrame.getUserReader().deleteMyConcert(i);
-            }
-             */
-            /*updateMyConcerts*/
             this.updateCheckedConcerts();
 
-            this.mainFrame.setNextPanelName(MainFrame.MyConcertsPanelName);
+            this.mainFrame.setNextPanelName(MainFrame.ConcertsPanelName);
 
         } else if (this.buttonAllConcerts.nowAction()) {
             this.mainFrame.setNextPanelName(MainFrame.ConcertsPanelName);
@@ -134,41 +126,37 @@ public class MyConcertsPanel extends JPanel implements ActionListener, Mediator 
 
     }
 
-    public void setMyConcertsTable(){
-        DefaultTableCellRenderer tableCellRenderer = new DefaultTableCellRenderer();
-        tableCellRenderer.setHorizontalAlignment(JLabel.CENTER);
-
-        for(int i = 1; i < myConcertsTable.getColumnModel().getColumnCount(); i++){
-            TableColumn col = myConcertsTable.getColumnModel().getColumn(i);
-            col.setCellRenderer(tableCellRenderer);
-        }
-
-        scrolleTable.setPreferredSize(new Dimension(PANEL_WIDTH,PANEL_HEIGHT/2));
-
-        //MyConcerts Table
-        Object[][] showMyConcertsOgject = this.mainFrame.getUserReader().getMyConcertsAsObject();
-
-        DefaultTableModel modeltable = new DefaultTableModel(COLUMN_NAMES,0){
-            public Class<?> getColumnClass(int columnIndex){
-                if(columnIndex == 0){
+    public void updateMyConcertsTable() {
+        DefaultTableModel modeltable = new DefaultTableModel(COLUMN_NAMES, 0) {
+            public Class<?> getColumnClass(int columnIndex) {
+                if (columnIndex == 0) {
                     return Boolean.class;
                 }
                 return super.getColumnClass(columnIndex);
             }
         };
 
-        for(var myConcertObject : showMyConcertsOgject){
-            modeltable.addRow(myConcertObject);
-            this.myConcertsTable.setModel(modeltable);
+        //MyConcerts Table
+        Object[][] showMyConcertsOgject = this.mainFrame.getUserReader().getMyConcertsAsObject();
+
+        for (int i = 0; i < showMyConcertsOgject.length; i++) {
+            showMyConcertsOgject[i][0] = !((boolean) showMyConcertsOgject[i][0]);
         }
+
+        for (var myConcertObject : showMyConcertsOgject) {
+            modeltable.addRow(myConcertObject);
+        }
+
+        this.myConcertsTable.setModel(modeltable);
     }
 
     public void updateCheckedConcerts() {
         ArrayList<ArrayList<String>> addedConcerts = new ArrayList<>();
         for (int i = 0; i < this.myConcertsTable.getRowCount(); i++) {
-            if (Boolean.parseBoolean(this.myConcertsTable.getValueAt(i, 0).toString())) {
+            if (!Boolean.parseBoolean(this.myConcertsTable.getValueAt(i, 0).toString())) {
                 ArrayList<String> addedConcert = new ArrayList<>();
-                for (int j = 0; j < this.myConcertsTable.getColumnCount(); j++) {
+                addedConcert.add("true");
+                for (int j = 1; j < this.myConcertsTable.getColumnCount(); j++) {
                     addedConcert.add(this.myConcertsTable.getValueAt(i, j).toString());
                 }
                 addedConcerts.add(addedConcert);
